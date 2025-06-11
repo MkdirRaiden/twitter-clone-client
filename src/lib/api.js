8a7ded2171156964f1befda6fe552aa2bf3343f5
Ahmed <1996raiden27@gmail.com>
2025-06-11 23:45:16 +0530

    error resolved

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 // Create the axios instance
 const api = axios.create({
     baseURL: import.meta.env.PROD
-        ? "https://your-backend-domain.com/api"
+        ? "https://twitter-clone-backend-f6w8.onrender.com"
         : "/api", // for local dev (proxied by Vite)
     withCredentials: true,
 });
